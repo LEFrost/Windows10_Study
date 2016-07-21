db6aa994dc3ac3526703cb795ef2e9144cfb0e8c
建 彭 <pengjian@live.cn>
2016-07-21 19:46:54 +0800

    .

--- Windows10_Study/MainPage.xaml.cs
+++ Windows10_Study/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -22,9 +23,26 @@ namespace Windows10_Study
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        string url = "http://apis.baidu.com/showapi_open_bus/oil_price/find";
         public MainPage()
         {
             this.InitializeComponent();
+        }
+        List< KeyValuePair<string,string>> Params;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string content="";
+            HttpClient httpClient = new HttpClient();
+            
+            Params.Add(new KeyValuePair<string, string>("prov",input.Text));
+            httpClient.DefaultRequestHeaders.Add("apikey", "6e4b2f5b59fc77691942c8ba6f3bf7e3");
+            HttpResponseMessage response= httpClient.GetAsync(new Uri(url)).Result;
+            
+            if(response.StatusCode==System.Net.HttpStatusCode.OK)
+            {
+                content = response.Content.ReadAsStringAsync().Result;
+            }
+            show.Text = content.ToString();
         }
     }
 }
